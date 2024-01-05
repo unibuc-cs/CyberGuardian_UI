0e58cc5bd9b8a134f2852b87e0e72a3482266410
AGAPIA <pciprian_ionut@yahoo.com>
2024-01-05 19:48:10 +0200

    User feedback, collection of data, many utilities added

--- Testgif.py
+++ Testgif.py
@@ -5,12 +5,12 @@ import base64
 st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
 
 """### gif from local file"""
-file_ = open("data/characters/talking_head1.gif", "rb")
+file_ = open("data/characters/boy.gif", "rb")
 contents = file_.read()
 data_url = base64.b64encode(contents).decode("utf-8")
 file_.close()
 
 st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" style="width:30px height:30px" alt="cat gif">',
+    f'<img src="data:image/gif;base64,{data_url}" width="200" height="200" style="opacity:0.4;filter:alpha(opacity=40);" alt="cat gif">',
     unsafe_allow_html=True,
 )
